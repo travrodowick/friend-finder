35edf0b9a14e383075ee1788b2d1e1449412f598
travis rodowick <travrodowick@yahoo.com>
2019-11-03 01:56:15 -0800

    first commit -- added server file and some other base elements -- base HTML routes and a file for API routes --- added a home page and a layout for the survey page ---- and added some friends data

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var friends = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+    // Note the code here. Our "server" will respond to requests
+    // req.body is available since we're using the body parsing middleware
+    //=============================================================
+    //it all goes here
+    //==============================================================
+  });
+
+  // ---------------------------------------------------------------------------
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+var path = require("path");
+
+// Initialize Express
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Routes
+//==================================================================
+require("./app/routing/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
